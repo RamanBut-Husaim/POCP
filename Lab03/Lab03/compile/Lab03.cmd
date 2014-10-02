e7e6192e59d4f6efc72da664ea55ca34cf84fb16
RamanBut-Husaim <raman_but-husaim@live.com>
2014-10-02 22:23:13 +0300

    [Lab03]: Add test bench for d enable trigger.

--- Lab03/Lab03/compile/Lab03.cmd
+++ Lab03/Lab03/compile/Lab03.cmd
@@ -1,1 +1,1 @@
--w -s "i:\Embedded\_Projects\POCP\Lab03\Lab03\library.cfg"  -j "i:\Embedded\_Projects\POCP\Lab03\Lab03\projlib.cfg"  -d "i:\Embedded\_Projects\POCP\Lab03\Lab03\compile"   -work Lab03  -dbg -e 100 -2002 "i:\Embedded\_Projects\POCP\Lab03\Lab03\src\Task9\JKFF.vhd"
+-w -s "i:\Embedded\_Projects\POCP\Lab03\Lab03\library.cfg"  -j "i:\Embedded\_Projects\POCP\Lab03\Lab03\projlib.cfg"  -d "i:\Embedded\_Projects\POCP\Lab03\Lab03\compile"   -work Lab03  -dbg -e 100 -2002 "i:\Embedded\_Projects\POCP\Lab03\Lab03\src\Testbenches\D_Enable_Latch_Test.vhd"
